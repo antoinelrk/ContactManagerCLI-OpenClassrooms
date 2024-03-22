de0711aab7ea66a2ca2ff448cf63f2f7404f3573
Antoine LRK <antoinelrk@icloud.com>
2024-03-22 09:55:18 +0100

    Change paths

--- includes/bootstrap.php
+++ includes/bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 
-include_once './Constants.php';
-include_once './core/Database.php';
+include_once 'Constants.php';
+include_once 'core/Database.php';
 
 $db = new Database(DB_HOST, DB_DATABASE, DB_USER, DB_PASSWORD);
 $connection = $db->getConnection();
